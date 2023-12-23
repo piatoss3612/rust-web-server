19a364b0f508f202bf0449b37414ec428479c316
Hyohwak Lee <crewe1746@naver.com>
2023-12-23 15:48:33 +0900

    Add users, roles and users roles table

---
+++ migrations/2023-12-23-062752_create_users_roles/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE users_roles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    role_id INTEGER NOT NULL REFERENCES roles(id)
+)
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,4 @@
-use crate::schema::{crates, rustaceans};
+use crate::schema::{crates, roles, rustaceans, users, users_roles};
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -41,4 +41,54 @@ pub struct NewCrate {
     pub name: String,
     pub version: String,
     pub description: Option<String>,
+}
+
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
+pub struct User {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name =users)]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
+pub struct Role {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub code: String,
+    pub name: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name =roles)]
+pub struct NewRole {
+    pub code: String,
+    pub name: String,
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Role))]
+pub struct UserRole {
+    pub id: i32,
+    pub role_id: i32,
+    pub user_id: i32,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name =users_roles)]
+pub struct NewUserRole {
+    pub role_id: i32,
+    pub user_id: i32,
 }
